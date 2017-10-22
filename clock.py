7f1fd642692419094cb8f6b3b8d284a77748c02d
Theriley106 <christopherlambert106@gmail.com>
2017-10-21 21:00:52 -0400

    Added flask server

--- clock.py
+++ clock.py
@@ -1,4 +1,27 @@
 import os
+import time
+import threading
+from time import gmtime, strftime
+from flask import Flask, request, render_template, request, url_for, redirect, Markup, Response, send_file, send_from_directory, make_response, jsonify
 
-class Clock(object):
-	
+
+'''class Clock(object):
+	def __init__(object):
+		strftime("%Y-%m-%d %H:%M:%S", gmtime())'''
+
+
+
+
+@app.route('/clear/', methods=['POST'])
+def clear():
+	os.system('sudo ./clear')
+
+@app.route('/write/<text>', methods=['POST'])
+def write():
+	text = text.split("/")
+	text = text.join(' ')
+	os.system('sudo ./main {}'.format(text))
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=8888)
