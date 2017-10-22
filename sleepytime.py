4dbca268836af7d52db3b73bae6c341220fc8a2a
Theriley106 <christopherlambert106@gmail.com>
2017-10-22 10:21:22 -0400

    fixed auth

--- sleepytime.py
+++ sleepytime.py
@@ -2,15 +2,14 @@ import requests
 import json
 import os
 
-
 def js_r(filename):
 	with open(filename) as f_in:
    		f_in = '{' + str(f_in.read()).partition('{')[2].strip()
     	return(json.loads(f_in))
 
 
-def getSleep(curl='curl -i -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzU0s2RlYiLCJhdWQiOiIyMkNKRFAiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTA5MjYyOTQ0LCJpYXQiOjE1MDg2NTgxNjh9.J63wQUZF0HOAhgk2e0r7fBm610VZJ3wJ0u6YZ5UJxkA" https://api.fitbit.com/1.2/user/-/sleep/date/2017-10-22.json')
-	os.system(curl + ' > t.json')
+def getSleep(auth='Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzU0s2RlYiLCJhdWQiOiIyMkNKRFAiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJhY3QgcnNldCBybG9jIHJ3ZWkgcmhyIHJudXQgcnBybyByc2xlIiwiZXhwIjoxNTA5MjYyOTQ0LCJpYXQiOjE1MDg2NTgxNjh9.J63wQUZF0HOAhgk2e0r7fBm610VZJ3wJ0u6YZ5UJxkA', url='https://api.fitbit.com/1.2/user/-/sleep/date/2017-10-22.json')
+	os.system('curl -i -H "{}" {} > t.json'.format(auth, url))
 	a = js_r('t.json')
 	if len(a['sleep']) == 0:
 		print("You should get some sleep")
