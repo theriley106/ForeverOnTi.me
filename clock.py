ec43efbb70140e9fdd9c0ea9f8ae3ecdaad24709
Theriley106 <christopherlambert106@gmail.com>
2017-10-21 21:13:03 -0400

    f

--- clock.py
+++ clock.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
 def clear():
 	os.system('sudo ./clear')
 	return "Done"
-
+	
+@app.route('/write/<text1>', methods=['POST'])
 @app.route('/write/<text1>/<text2>', methods=['POST'])
 def write(text1, text2=""):
 	os.system('sudo ./main {} {}'.format(text1, text2))
