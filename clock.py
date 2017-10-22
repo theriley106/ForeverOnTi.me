53a574d06d3c68006c4f4b56072d32cee6854b67
Theriley106 <christopherlambert106@gmail.com>
2017-10-22 03:09:19 -0400

    f

--- clock.py
+++ clock.py
@@ -30,7 +30,7 @@ class Clock(object):
 		while True:
 			if self.custom == False:
 				#a = strftime("%H:%M:%S %Y-%m-%d", gmtime()).replace(' ', '/')
-				a = (datetime.datetime.utcnow() - datetime.timedelta(hours=4))
+				a = str((datetime.datetime.utcnow() - datetime.timedelta(hours=4)))
 				a, b = a.split(' ')
 				b = b.partition('.')[0]
 				os.system('sudo ./main "{}" "{}"'.format(b, a))
